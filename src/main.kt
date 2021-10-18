dc2b65bf2aa2d8ad7e8945457d82f95a4a424b09
3minbb <36643588+3minbb@users.noreply.github.com>
2021-10-18 18:17:53 +0300

    Update main.kt

--- src/main.kt
+++ src/main.kt
@@ -1,9 +1,16 @@
 fun main(){
-    val init = Shop("UK")
-    val obj_1 = Yachmen(3, true)
-    val obj_2 = Roz(12,true)
-    init.addItem(obj_1)
-    init.addItem(obj_2)
-    init.sellerinfo()
+    val init = Shop(
+        "EN"
+    )
 
+    init.addItem(Roz(
+        35,
+        true,
+    ))
+
+    init.addItem(Roz(
+        11,
+        false,
+    ))
+    println(init.listNEW)
 }
